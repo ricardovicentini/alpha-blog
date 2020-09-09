c2e8c830c2486af0d084d883725d4e8a41fbd7ef
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2020-09-09 10:22:32 -0300

    refactoring repetitions in the article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
   
   
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def new
@@ -13,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title,:description)) 
+    @article = Article.new(article_params) 
     if @article.save
       flash[:notice] = "Article saved succefuly"
       redirect_to @article
@@ -23,12 +25,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title,:description))
+    
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -37,10 +39,19 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
   
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
   
+
 end
